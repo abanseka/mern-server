4331d173e0f2297f8ebd1588f28e50925a872390
sly <sylvester@archis.company>
2023-03-14 09:54:20 +0100

    FIX: fixed user auth issues ie registration, login

--- controllers/auth.js
+++ controllers/auth.js
@@ -11,7 +11,7 @@ export const register = async (req, res) => {
       email,
       password,
       picturePath,
-      freinds,
+      friends,
       location,
       occupation,
     } = req.body;
@@ -25,18 +25,18 @@ export const register = async (req, res) => {
       email,
       password: passwordHash,
       picturePath,
-      freinds,
+      friends,
       location,
       occupation,
-      viewedProfile: Math.floor(Math.random() * 1000),
+      viewedProfile: Math.floor(Math.random() * 10000),
       impressions: Math.floor(Math.random() * 10000),
     });
 
     const savedUser = await newUser.save();
-    res.statu(201).json(savedUser);
+    res.status(201).json(savedUser);
     ///////////////////////////////
   } catch (err) {
-    res.status(500).json({ err: err.message });
+    res.status(500).json({ error: err.message });
   }
 };
 
@@ -44,18 +44,18 @@ export const register = async (req, res) => {
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
-    const user = await User.findOne({ email });
-    if (!user) return res.status(400).json({ message: "User does not exist." });
+    const user = await User.findOne({ email: email });
+    if (!user) return res.status(400).json({ msg: "User does not exist. " });
 
     const isMatch = await bcrypt.compare(password, user.password);
     // prettier-ignore
-    if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
+    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. " });
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
     delete user.password;
     res.status(200).json({ token, user });
     //////////////////////////////////////
   } catch (err) {
-    res.status(500).json({ err: err.message });
+    res.status(500).json({ error: err.message });
   }
 };
